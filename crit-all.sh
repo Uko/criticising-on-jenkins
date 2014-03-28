8091dfc120341bd74d4b0709e9b918e0ab54bf6f
Uko <uko@unikernel.net>
2014-03-28 10:39:13 +0100

    Renamed *-port.csv to *-report.csv

--- crit-all.sh
+++ crit-all.sh
@@ -1,6 +1,6 @@
-echo 'Code Critics,Renraku' > all-count-port.csv
+echo 'Code Critics,Renraku' > all-count-report.csv
 
-./pharo Renraku.image analyse --number >> all-count-port.csv
-echo -n , >> all-count-port.csv
+./pharo Renraku.image analyse --number >> all-count-report.csv
+echo -n , >> all-count-report.csv
 ./pharo Renraku.image eval --save 'REPrefs default useAllPackages'
-./pharo Renraku.image eval "RERunner newWithDefault run numberOfImportantReports" >> all-count-port.csv
+./pharo Renraku.image eval "RERunner newWithDefault run numberOfImportantReports" >> all-count-report.csv
--- crit-ren.sh
+++ crit-ren.sh
@@ -1,5 +1,5 @@
-echo 'Code Critics,Renraku' > re-count-port.csv
+echo 'Code Critics,Renraku' > re-count-report.csv
 
-./pharo Renraku.image analyse --packages='Renraku' --number >> re-count-port.csv
-echo -n , >> re-count-port.csv
-./pharo Renraku.image eval "RERunner newWithDefault run numberOfImportantReports" >> re-count-port.csv
+./pharo Renraku.image analyse --packages='Renraku' --number >> re-count-report.csv
+echo -n , >> re-count-report.csv
+./pharo Renraku.image eval "RERunner newWithDefault run numberOfImportantReports" >> re-count-report.csv
